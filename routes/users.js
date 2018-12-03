597c304a19378291a9b6ff6fc524ae7871bfe763
Olumide <olumideokedusi@gmail.com>
2018-12-03 05:29:32 +0100

    define post and get routes to handle adding user to the db

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,33 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//Users Index
+router.get("/", (req, res, next) => {
+  User.find({}, "username", (err, users) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("users/index", { users });
+    }
+  });
+});
+
+//Users new
+router.get("/new", (req, res, next) => {
+  res.render("users/new");
+});
+
+//Users create
+router.post("/", (req, res, next) => {
+  const user = new User(req.body);
+  user.save((err, user) => {
+    if (err) {
+      console.log(err);
+    } else {
+      return res.redirect("/users");
+    }
+  });
 });
 
 module.exports = router;
