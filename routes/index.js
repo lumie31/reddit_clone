ef7d94e8cea5f15ef14458d79b73d2f27bdf18f2
Olumide <olumideokedusi@gmail.com>
2018-12-04 05:05:16 +0100

    Define post route for login action

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Reddit, Clone" });
+});
+
+//get login page
+router.get("/login", (req, res, next) => {
+  res.render("login");
+});
+
+//login route
+router.post("/login", (req, res, next) => {
+  User.authenticate(req.body.username, req.body.password, (err, user) => {
+    if (err || !user) {
+      const next_error = new Error("Username or password incorrect");
+      next_error.status = 401;
+
+      return next(next_error);
+    } else {
+      req.session.userId = user._id;
+
+      return res.redirect("/");
+    }
+  });
 });
 
 module.exports = router;
