664d7681361947d89af30fe6d7466b2dc749cf17
Olumide <olumideokedusi@gmail.com>
2018-12-04 10:37:33 +0100

    Implement Logout functionality

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,17 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 
+// set layout variables
+router.use(function(req, res, next) {
+  res.locals.title = "Reddit, Clone";
+  res.locals.currentUserId = req.session.userId;
+
+  next();
+});
+
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Reddit, Clone" });
+  res.render("index");
 });
 
 //get login page
@@ -14,6 +22,7 @@ router.get("/login", (req, res, next) => {
 
 //login route
 router.post("/login", (req, res, next) => {
+  console.log(req.session);
   User.authenticate(req.body.username, req.body.password, (err, user) => {
     if (err || !user) {
       const next_error = new Error("Username or password incorrect");
@@ -26,6 +35,16 @@ router.post("/login", (req, res, next) => {
       return res.redirect("/");
     }
   });
+});
+
+//logout
+router.get("/logout", (req, res, next) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) return next(err);
+    });
+  }
+  return res.redirect("/");
 });
 
 module.exports = router;
