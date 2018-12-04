f6e550d32ae1b7c94bb87a45b6c682348ab87575
Olumide <olumideokedusi@gmail.com>
2018-12-04 04:09:53 +0100

    Hash password using bcrypt

--- models/user.js
+++ models/user.js
@@ -1,9 +1,23 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
 const UserSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true }
+});
+
+UserSchema.pre("save", function(next) {
+  let user = this;
+  bcrypt.hash(user.password, 10, function(err, hash) {
+    // Store hash in your password DB.
+    if (err) {
+      return next(err);
+    } else {
+      user.password = hash;
+      next();
+    }
+  });
 });
 
 const User = mongoose.model("User", UserSchema);
